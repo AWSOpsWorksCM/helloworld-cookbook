59d7538ddc9501f859cb2c24b6914ecb38ef31c1
Cochran <kevco@amazon.com>
2018-05-11 15:15:10 -0700

    Initial commit

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'helloworld'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Kevin Cochran and Sean Carolan'
+maintainer_email 'kevco@example.com'
 license 'all_rights'
 description 'Installs/Configures helloworld'
 long_description 'Installs/Configures helloworld'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,25 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+# Install a package!
+package 'httpd' do
+  action :install
+end
+
+# Configure a file
+cookbook_file '/var/www/html/index.html' do
+  action :create
+  source 'index.html'
+end
+
+# Configure an image
+cookbook_file '/var/www/html/amazon.png' do
+  action :create
+  source 'amazon.png'
+end
+
+# Start the Apache service
+service 'httpd' do
+  action :start
+end
